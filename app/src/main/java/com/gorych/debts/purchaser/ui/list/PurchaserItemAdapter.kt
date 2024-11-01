4d6bf4aaba0c369cf16222f3855d7890d9b44533
Yahor Semianchenia <gorych911@gmail.com>
2024-11-01 22:03:27 +0300

    Minor refactoring of alignments

--- app/src/main/java/com/gorych/debts/purchaser/ui/list/PurchaserItemAdapter.kt
+++ app/src/main/java/com/gorych/debts/purchaser/ui/list/PurchaserItemAdapter.kt
@@ -11,6 +11,7 @@ import com.gorych.debts.adapter.BaseViewHolder
 import com.gorych.debts.purchaser.IntentExtras
 import com.gorych.debts.purchaser.Purchaser
 import com.gorych.debts.purchaser.ui.detail.ClientDetailActivity
+import com.gorych.debts.utility.hide
 
 class PurchaserItemAdapter :
     BaseAdapter<Purchaser, PurchaserItemAdapter.PurchaserItemViewHolder>() {
@@ -26,37 +27,39 @@ class PurchaserItemAdapter :
 
     inner class PurchaserItemViewHolder(view: View) : BaseViewHolder<Purchaser>(view) {
 
-        private val clientPhoneView: TextView = view.findViewById(R.id.all_clients_item_tv_phone)
-        private val clientDebtsView: TextView = view.findViewById(R.id.all_clients_item_tv_debts)
+        private val clientPhoneView: TextView =
+            itemView.findViewById(R.id.all_clients_item_tv_phone)
+        private val clientDebtsView: TextView =
+            itemView.findViewById(R.id.all_clients_item_tv_debts)
         private val clientFullNameView: TextView =
-            view.findViewById(R.id.all_clients_item_tv_full_name)
+            itemView.findViewById(R.id.all_clients_item_tv_full_name)
 
         override fun bind(item: Purchaser) {
-            val activityContext = itemView.context
+            val itemViewContext = itemView.context
             clientFullNameView.text = item.fullName()
             when {
                 item.phoneNumber != null -> {
                     clientPhoneView.text =
-                        activityContext.getString(
+                        itemViewContext.getString(
                             R.string.client_phone_view_prefix,
                             item.phoneNumber
                         )
                 }
 
                 else -> {
-                    clientPhoneView.visibility = View.GONE
+                    clientPhoneView.hide()
                 }
             }
 
             clientDebtsView.text =
-                if (item.hasActiveDebts()) activityContext.getString(R.string.client_debts_text_view_YES_value)
-                else activityContext.getString(R.string.client_debts_text_view_NO_value)
+                if (item.hasActiveDebts()) itemViewContext.getString(R.string.client_debts_text_view_YES_value)
+                else itemViewContext.getString(R.string.client_debts_text_view_NO_value)
 
             itemView.setOnClickListener {
-                val intent = Intent(activityContext, ClientDetailActivity::class.java).apply {
+                val intent = Intent(itemViewContext, ClientDetailActivity::class.java).apply {
                     putExtra(IntentExtras.SELECTED_PURCHASER, item)
                 }
-                activityContext.startActivity(intent)
+                itemViewContext.startActivity(intent)
             }
         }
     }
