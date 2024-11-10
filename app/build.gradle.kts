abfcc201b28b33868eb85fac34f24573df71a440
Yahor Semianchenia <gorych911@gmail.com>
2024-11-10 12:57:23 +0300

    Added ksp and room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.gorych.debts"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.gorych.debts"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
