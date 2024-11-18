ca55da18ace5952f05017d8fa188738d6536ca6a
Yahor Semianchenia <gorych911@gmail.com>
2024-11-19 00:10:58 +0300

    Removed barcode related fragment and associated things

--- app/src/main/java/com/gorych/debts/barcode/BarcodeField.kt
+++
@@ -1,8 +0,0 @@
-package com.gorych.debts.barcode
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-/** Information about a barcode field.  */
-@Parcelize
-data class BarcodeField(val label: String, val value: String) : Parcelable
