a32d5b5bcc43344f1487bc0f61d017312a068fbc
Yahor Semianchenia <gorych911@gmail.com>
2024-11-19 23:13:33 +0300

    Updated logic to crop bitmap

--- app/src/main/java/com/gorych/debts/utility/BitmapUtils.kt
+++ app/src/main/java/com/gorych/debts/utility/BitmapUtils.kt
@@ -1,7 +1,9 @@
 package com.gorych.debts.utility
 
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Rect
+import com.gorych.debts.good.Good
 import java.io.ByteArrayOutputStream
 
 object BitmapUtils {
@@ -12,13 +14,33 @@ object BitmapUtils {
         return stream.toByteArray()
     }
 
-    fun cropBitmap(originalBitmap: Bitmap, cropArea: Rect): Bitmap {
+    fun cropBitmap(
+        originalBitmap: Bitmap,
+        barcodeBoundingBox: Rect,
+        imgViewHeight: Int
+    ): Bitmap {
+        val heightRatio = 1.7f //TODO might differ on different screens
+        val croppedImgHeight: Int = Math.round(heightRatio * imgViewHeight)
+
+        val boundingBoxCenterY: Int = Math.round(barcodeBoundingBox.top / 2f)
+        val croppedImgRatioY = 1.3f
+        val croppedImgTop = Math.round(croppedImgRatioY * (boundingBoxCenterY + imgViewHeight / 2))
+
         return Bitmap.createBitmap(
             originalBitmap,
-            cropArea.left,
-            cropArea.top,
-            cropArea.width(),
-            cropArea.height()
+            0,
+            croppedImgTop,
+            originalBitmap.width,
+            croppedImgHeight
         )
     }
+
+    fun convertBytesToBitmap(data: ByteArray?): Bitmap? =
+        BitmapFactory.decodeByteArray(
+            data,
+            0,
+            data?.size ?: 0
+        )
+
+    fun createBitmapFromGood(good: Good): Bitmap? = convertBytesToBitmap(good.imageData)
 }
