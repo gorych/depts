97e3a32d2ad9027de4fbb345c283a76e3ddd065a
Yahor Semianchenia <gorych911@gmail.com>
2025-01-12 13:02:19 +0300

    Fixed keyboard issue for dropdown menu

--- app/src/main/java/com/gorych/debts/good/ui/add/AddGoodActivity.kt
+++ app/src/main/java/com/gorych/debts/good/ui/add/AddGoodActivity.kt
@@ -3,6 +3,7 @@ package com.gorych.debts.good.ui.add
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.EditText
@@ -33,6 +34,7 @@ import com.gorych.debts.purchaser.IntentExtras
 import com.gorych.debts.utility.clearText
 import com.gorych.debts.utility.textAsString
 import kotlinx.coroutines.launch
+
 
 class AddGoodActivity : TopBarActivityBase() {
 
@@ -177,6 +179,17 @@ class AddGoodActivity : TopBarActivityBase() {
                 }
             }
         })
+
+        dropdownMeasurementUnit.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                hideKeyboard(v);
+            }
+        }
+    }
+
+    private fun hideKeyboard(view: View) {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     private fun showQuestionDialog() {
