b78cd4d88a2ddbbcc588776b0dd9f85307b9c768
Yahor Semianchenia <gorych911@gmail.com>
2026-01-28 10:29:41 +0300

    Updated test data in PurchaserDebtRepository

--- app/src/main/java/com/gorych/debts/debt/repository/PurchaserDebtRepository.kt
+++ app/src/main/java/com/gorych/debts/debt/repository/PurchaserDebtRepository.kt
@@ -19,8 +19,16 @@ class PurchaserDebtRepository {
         )
 
         val debts = listOf(
-            Debt(1, "Чек №1", now(), Status.OPEN, purchaser, goods, "Алла"),
-            Debt(2, "Чек №2", now(), Status.CLOSED, purchaser, goods, "Наташа"),
+            Debt(1, "Чек №101", now(), Status.OPEN, purchaser, goods, "Алла"),
+            Debt(2, "Чек №233", now().minusDays(1), Status.CLOSED, purchaser, goods, "Наташа"),
+            Debt(3, "Чек №244", now().minusDays(2), Status.OPEN, purchaser, goods, "Ольга"),
+            Debt(4, "Чек №245", now().minusDays(3), Status.CLOSED, purchaser, goods, "Наташа"),
+            Debt(5, "Чек №301", now().minusDays(4), Status.OPEN, purchaser, goods, "Наташа"),
+            Debt(6, "Чек №308", now().minusDays(5), Status.CLOSED, purchaser, goods, "Наташа"),
+            Debt(7, "Чек №443", now().minusDays(6), Status.OPEN, purchaser, goods, "Ольга"),
+            Debt(8, "Чек №456", now().minusDays(7), Status.CLOSED, purchaser, goods, "Виктория"),
+            Debt(9, "Чек №789", now().minusDays(8), Status.OPEN, purchaser, goods, "Алла"),
+            Debt(10, "Чек №901", now().minusDays(9), Status.CLOSED, purchaser, goods, "Алла"),
         )
         return debts
     }
