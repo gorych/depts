9ba546e99b3903608bd740fae57d90d5db1a1a14
Yahor Semianchenia <gorych911@gmail.com>
2026-01-29 15:47:54 +0300

    Removed debt related classes

--- app/src/main/java/com/gorych/debts/purchaser/presenter/PurchaserDetailPresenter.kt
+++ app/src/main/java/com/gorych/debts/purchaser/presenter/PurchaserDetailPresenter.kt
@@ -1,21 +1,21 @@
 package com.gorych.debts.purchaser.presenter
 
-import com.gorych.debts.debt.repository.PurchaserDebtRepository
+import com.gorych.debts.receipt.repository.ReceiptRepository
 import com.gorych.debts.purchaser.Purchaser
 import com.gorych.debts.purchaser.contract.PurchaserDetailContract
 
 class PurchaserDetailPresenter(
     private val view: PurchaserDetailContract.View,
-    private val purchaserDebtRepository: PurchaserDebtRepository,
+    private val receiptRepository: ReceiptRepository,
 ) : PurchaserDetailContract.Presenter {
 
     override suspend fun loadActiveDebts(purchaser: Purchaser) {
-        val debts = purchaserDebtRepository.getActiveDebtsOfPurchaser(purchaser)
+        val debts = receiptRepository.getActiveDebtsOfPurchaser(purchaser)
         view.populateDebts(debts)
     }
 
     override suspend fun loadAllDebts(purchaser: Purchaser) {
-        val debts = purchaserDebtRepository.getAllDebtsOfPurchaser(purchaser)
+        val debts = receiptRepository.getAllDebtsOfPurchaser(purchaser)
         view.populateDebts(debts)
     }
 
