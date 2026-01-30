7fae48d92edb6931c30ac44aeab64e7a1585a88b
Yahor Semianchenia <gorych911@gmail.com>
2026-01-30 11:40:31 +0300

    Added string templates for debt fields

--- app/src/main/java/com/gorych/debts/purchaser/ui/detail/DebtRecyclerViewAdapter.kt
+++ app/src/main/java/com/gorych/debts/purchaser/ui/detail/DebtRecyclerViewAdapter.kt
@@ -1,5 +1,6 @@
 package com.gorych.debts.purchaser.ui.detail
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,10 +8,10 @@ import android.widget.TextView
 import com.gorych.debts.R
 import com.gorych.debts.core.adapter.BaseAdapter
 import com.gorych.debts.core.adapter.BaseViewHolder
-import com.gorych.debts.debt.Debt
+import com.gorych.debts.receipt.Receipt
 
 class DebtRecyclerViewAdapter :
-    BaseAdapter<Debt, DebtRecyclerViewAdapter.DebtItemViewHolder>() {
+    BaseAdapter<Receipt, DebtRecyclerViewAdapter.DebtItemViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DebtItemViewHolder {
         return DebtItemViewHolder(
@@ -21,16 +22,28 @@ class DebtRecyclerViewAdapter :
         )
     }
 
-    inner class DebtItemViewHolder(itemView: View) : BaseViewHolder<Debt>(itemView) {
+    class DebtItemViewHolder(itemView: View) : BaseViewHolder<Receipt>(itemView) {
+        val itemViewContext: Context = itemView.context
+
         private val debtNameView: TextView = itemView.findViewById(R.id.client_info_tv_debt_name)
-        private val debtSellerView: TextView = itemView.findViewById(R.id.client_info_tv_debt_seller)
+        private val debtSellerView: TextView =
+            itemView.findViewById(R.id.client_info_tv_debt_seller)
         private val debtCreationDateView: TextView =
             itemView.findViewById(R.id.client_info_tv_debt_creation_date)
 
-        override fun bind(item: Debt) {
-            debtNameView.text = item.name
-            debtSellerView.text = item.seller
-            debtCreationDateView.text = item.created.toString()
+        override fun bind(item: Receipt) {
+            debtNameView.text = itemViewContext.getString(
+                R.string.receipt_number_template_string,
+                item.id
+            )
+            debtSellerView.text = itemViewContext.getString(
+                R.string.seller_template_string,
+                item.seller
+            )
+            debtCreationDateView.text = itemViewContext.getString(
+                R.string.created_at_template_string,
+                item.createdAtFormatted
+            )
         }
     }
 }
